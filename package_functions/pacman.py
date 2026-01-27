97a918153e495f24eea73543a2fddecce2656413
xblade720 <xblade720.gamer@proton.me>
2026-01-27 21:07:27 +0100

    fixed sudo calls

--- package_functions/pacman.py
+++ package_functions/pacman.py
@@ -6,16 +6,8 @@ def db_update():
 def full_upgrade():
     subprocess.call(['sudo', 'pacman', '-Syu'])
 
-def safe_upgrade():         # If you have a better name for this variable, i'm not against it
-    subprocess.call(['sudo', 'pacman', '-Syyu'])
-
 def package_remove(package):
     subprocess.call(['sudo', 'pacman', '-R', package])
 
 def package_install(package):
-    status = subprocess.run(['sudo', 'pacman', '-S', package], capture_output=True)
-    if status.returncode == 1:
-        print("Package was not found in local database. Searching in online database")
-        status = subprocess.run(['sudo', 'pacman', '-Sy', package])
-        if status.returncode == 1:
-            print("The package you requested does not exit in your active repositories")
+    subprocess.call(['sudo', 'pacman', '-Sy', package])
